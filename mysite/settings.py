4677f09e776e6ac77be471e42caa000085e52245
Abdul Samad <ranasamad074@gmail.com>
2026-08-25 10:04:07 +0500

    complete first 3 blocks of day 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'polls',
     'notes',
+    'shop',
 ]
 
 MIDDLEWARE = [
---
+++ shop/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Tag(models.Model):
+    name=models.CharField(max_length=50)
+
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    tags=models.ManyToManyField(Tag, through='Product_Tag', blank=True, related_name='products')
+
+class Product_Tag(models.Model):
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    tag=models.ForeignKey(Tag, on_delete=models.CASCADE)
+    added_at=models.DateTimeField(auto_now_add=True)
+    
+class Listing(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    seller=models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="listings_selling")
+    buyer=models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="listings_buying")
+    created_at=models.DateTimeField(auto_now_add=True)
