2bdf46a194637097d0a5cc1dc401a339068b618d
Abdul Samad <ranasamad074@gmail.com>
2026-08-27 15:08:57 +0500

    day 2 blocks completed

--- shop/models.py
+++ shop/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     name=models.CharField(max_length=100)
     price=models.DecimalField(max_digits=10, decimal_places=2)
     tags=models.ManyToManyField(Tag, through='Product_Tag', blank=True, related_name='products')
-
+    sku = models.CharField(max_length=50,null=True)
 class Product_Tag(models.Model):
     product=models.ForeignKey(Product, on_delete=models.CASCADE)
     tag=models.ForeignKey(Tag, on_delete=models.CASCADE)
@@ -20,3 +20,8 @@ class Listing(models.Model):
     seller=models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="listings_selling")
     buyer=models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="listings_buying")
     created_at=models.DateTimeField(auto_now_add=True)
+
+class PriceHistory(models.Model):
+    Listing=models.ForeignKey(Listing, on_delete=models.CASCADE,related_name='price_histories'  )
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp=models.DateTimeField(auto_now_add=True)
